594690ff184d8882c40782af69d21cc42226c333
Andy Le <leal1@uci.edu>
2018-12-21 14:20:11 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Routes(){
+    return(
+        <Router>
+            <div>
+                <Link to="/new"> New Post </Link>
+                <Route exact path='/' component={Board}/>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+                <Route path="/new" component={submitPost}/>
+            </div>
+
+       
+        </Router>
+    );
+}
+
+class submitPost extends React.Component{
+    render(){
+        return(
+            <div class="submitPost">
+              <form>
+                <label>Title: </label>
+                <input type="text" name="title"/>
+                <input type="submit" value="Submit!"/>
+              </form>
+
+            </div>
+        )
+    }
+}
+
+class Post extends React.Component{
+ 
+    render(){
+        return(
+            <div className="post">
+                 <hr/>
+                <div className="upvote"> upvote </div>
+                <div className="downvote"> downvote </div>
+                <div className="post-body">
+                    <h1 className="title"> {this.props.title} </h1>
+                    <p className="submitted">Submitted 10 days ago...</p>
+                </div>
+
+            </div>
+
+        )
+    }
+}
+
+class Board extends React.Component{
+    render(){
+        let posts = [];
+        for(let i=1; i < 11; i++){
+            posts.push(<Post title={"Post Title " + i.toString()}/>)
+        }
+        return(
+            posts
+        )
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Routes/>,
+    document.getElementById('root')
+  );
